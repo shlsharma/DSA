d5b77dd638611acd2e3da848ae540b3b1a7d4d1c
shlsharma <shlsharma.sharma@hotmail.com>
2024-08-30 18:37:29 +0530

    update

--- Array_List/10_rotate_matrix_image.py
+++ Array_List/10_rotate_matrix_image.py
@@ -16,5 +16,25 @@ Output:         [[7,4,1],
                  [8,5,2],
                  [9,6,3]]
 '''
+import numpy as np
+
 def rotate_matrix(matrix):
-    
+    matt = []
+    for i in range(len(matrix)):
+        mat = []
+        for j in range(len(matrix)-1, -1, -1):
+            mat.append(matrix[j][i])
+        matt.append(mat)
+    return matt
+
+# def rotate_matrix(matrix):
+#     matrix = np.array(matrix)
+#     matrix = matrix.T
+#     for i in range(np.shape(matrix)[0]):
+#         temp = matrix[i][::-1]
+#         matrix[i] = temp
+#     return matrix
+
+print(rotate_matrix([[1,2,3],
+                 [4,5,6],
+                 [7,8,9]]))
